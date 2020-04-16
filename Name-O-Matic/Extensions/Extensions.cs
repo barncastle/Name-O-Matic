daa9639c64ae8d98ba48e1cbade3dbfd6b5842f9
barncastle <spearman.tom@gmail.com>
2020-04-16 15:18:15 +0100

    fix logging

--- Name-O-Matic/Extensions/Extensions.cs
+++ Name-O-Matic/Extensions/Extensions.cs
@@ -7,13 +7,11 @@ namespace NameOMatic.Extensions
 {
     static class Extensions
     {
-        public static void StopAndLog(this Stopwatch sw, string format, int? left = null, int? top = null)
+        public static void StopAndLog(this Stopwatch sw, string format, int left, int top)
         {
             sw.Stop();
-            var (CursorLeft, CursorTop) = (Console.CursorLeft, Console.CursorTop);
-            Console.SetCursorPosition(left ?? CursorLeft, top ?? CursorTop);
+            Console.SetCursorPosition(left, top);
             Console.WriteLine($"Started {format} enumeration... completed in {sw.Elapsed}");
-            Console.SetCursorPosition(CursorLeft, CursorTop);
         }
 
         public static string ToToken(this IffToken token, bool reverse = false)
