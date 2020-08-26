5b217245069d18634192541c13bc350b33fc4d0d
barncastle <spearman.tom@gmail.com>
2020-08-26 21:38:35 +0100

    fixes and updates

--- Name-O-Matic/Formats/WDT/WDTModel.cs
+++ Name-O-Matic/Formats/WDT/WDTModel.cs
@@ -28,21 +28,23 @@ namespace NameOMatic.Formats.WDT
             string map = Directory;
             var listfile = ListFile.Instance;
 
-            if (!listfile.ContainsKey(FileDataId))
+            bool IsValidReplacement(int fid) => !listfile.ContainsKey(fid) || listfile[fid].Contains("unknown");
+
+            if (IsValidReplacement(FileDataId))
                 FileNames[FileDataId] = name + ".wdt";
-            if (!listfile.ContainsKey(Header.LgtFile))
+            if (IsValidReplacement(Header.LgtFile))
                 FileNames[Header.LgtFile] = name + "_lgt.wdt";
-            if (!listfile.ContainsKey(Header.OccFile))
+            if (IsValidReplacement(Header.OccFile))
                 FileNames[Header.OccFile] = name + "_occ.wdt";
-            if (!listfile.ContainsKey(Header.FogsFile))
+            if (IsValidReplacement(Header.FogsFile))
                 FileNames[Header.FogsFile] = name + "_fogs.wdt";
-            if (!listfile.ContainsKey(Header.MpvFile))
+            if (IsValidReplacement(Header.MpvFile))
                 FileNames[Header.MpvFile] = name + "_mpv.wdt";
-            if (!listfile.ContainsKey(Header.TexFile))
+            if (IsValidReplacement(Header.TexFile))
                 FileNames[Header.TexFile] = name + ".tex";
-            if (!listfile.ContainsKey(Header.WdlFile))
+            if (IsValidReplacement(Header.WdlFile))
                 FileNames[Header.WdlFile] = name + ".wdl";
-            if (!listfile.ContainsKey(Header.PD4File))
+            if (IsValidReplacement(Header.PD4File))
                 FileNames[Header.PD4File] = name + ".pd4";
 
             int i, j;
@@ -51,21 +53,21 @@ namespace NameOMatic.Formats.WDT
                 MAID maid = MapIDs[m];
                 i = m % 64; j = m / 64;
 
-                if (!listfile.ContainsKey(maid.RootADT))
+                if (IsValidReplacement(maid.RootADT))
                     FileNames[maid.RootADT] = name + $"_{i}_{j}.adt";
-                if (!listfile.ContainsKey(maid.Obj0ADT))
+                if (IsValidReplacement(maid.Obj0ADT))
                     FileNames[maid.Obj0ADT] = name + $"_{i}_{j}_obj0.adt";
-                if (!listfile.ContainsKey(maid.Obj1ADT))
+                if (IsValidReplacement(maid.Obj1ADT))
                     FileNames[maid.Obj1ADT] = name + $"_{i}_{j}_obj1.adt";
-                if (!listfile.ContainsKey(maid.Tex0ADT))
+                if (IsValidReplacement(maid.Tex0ADT))
                     FileNames[maid.Tex0ADT] = name + $"_{i}_{j}_tex0.adt";
-                if (!listfile.ContainsKey(maid.LodADT))
+                if (IsValidReplacement(maid.LodADT))
                     FileNames[maid.LodADT] = name + $"_{i}_{j}_lod.adt";
-                if (!listfile.ContainsKey(maid.MapTexture))
+                if (IsValidReplacement(maid.MapTexture))
                     FileNames[maid.MapTexture] = $"world/maptextures/{map}/{map}_{i:D2}_{j:D2}.blp";
-                if (!listfile.ContainsKey(maid.MapTextureN))
+                if (IsValidReplacement(maid.MapTextureN))
                     FileNames[maid.MapTextureN] = $"world/maptextures/{map}/{map}_{i:D2}_{j:D2}_n.blp";
-                if (!listfile.ContainsKey(maid.MinimapTexture))
+                if (IsValidReplacement(maid.MinimapTexture))
                     FileNames[maid.MinimapTexture] = $"world/minimaps/{map}/map{i:D2}_{j:D2}.blp";
             }
         }
