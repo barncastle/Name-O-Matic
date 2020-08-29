e4a901456461109e425df4fa60788d5e4f954abf
barncastle <spearman.tom@gmail.com>
2020-08-29 13:15:17 +0100

    use wow.tools tactkey page

--- Name-O-Matic/Database/Lookups/TactKeyLookup.cs
+++ Name-O-Matic/Database/Lookups/TactKeyLookup.cs
@@ -1,5 +1,7 @@
 ﻿using System.Globalization;
 using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
 using CASCLib;
 
 namespace NameOMatic.Database.Lookups
@@ -9,14 +11,28 @@ namespace NameOMatic.Database.Lookups
         public TactKeyLookup() : base(Path.Combine("Lookups", "TactKeyLookup.txt"))
         {
             Populate();
+            ScrapeWoWTools();
         }
 
         protected override void ParseLine(string line)
         {
             string[] parts = line.Split(';');
 
-            if (ulong.TryParse(parts[0], NumberStyles.HexNumber, null, out var key) && !parts[1].Contains('?'))
+            if (parts.Length == 2 && ulong.TryParse(parts[0], NumberStyles.HexNumber, null, out var key) && !parts[1].Contains('?'))
                 this[key] = parts[1].ToByteArray();
+        }
+
+        private void ScrapeWoWTools()
+        {
+            var wc = new WebClient();
+            var source = wc.DownloadString("https://wow.tools/dbc/tactkey.php");
+            var regex = new Regex("\"keyname\":\"([A-F0-9]{16})\",\"keybytes\":\"([A-F0-9]{32})\"");
+
+            foreach (Match match in regex.Matches(source))
+            {
+                if (match.Success && ulong.TryParse(match.Groups[1].Value, NumberStyles.HexNumber, null, out var key) && !match.Groups[2].Value.Contains('?'))
+                    this[key] = match.Groups[2].Value.ToByteArray();
+            }
         }
     }
 }
