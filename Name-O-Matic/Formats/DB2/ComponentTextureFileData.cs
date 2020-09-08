d476f221d59af1d26c81d07ed276850ef2359524
barncastle <spearman.tom@gmail.com>
2020-09-08 20:19:55 +0100

    fix

--- Name-O-Matic/Formats/DB2/ComponentTextureFileData.cs
+++ Name-O-Matic/Formats/DB2/ComponentTextureFileData.cs
@@ -81,7 +81,7 @@ namespace NameOMatic.Formats.DB2
                 if (!SectionPaths.TryGetValue(rec.Section, out string path))
                     continue;
 
-                FileNames[rec.FileDataId] = $"{path}{collection}_{slot}_{rec.Section}_{rec.Gender}{rec.TextureSuffix}.blp".Replace("__", "_");
+                FileNames[rec.FileDataId] = $"{path}{collection}_{slot}_{rec.Section}_{rec.Gender}{rec.TextureSuffix}_{rec.FileDataId}.blp".Replace("__", "_");
             }
         }
 
